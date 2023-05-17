d2dbadf7d575cb1e3e18ccc38768d4db64902d61
Stefano Pigozzi <me@steffo.eu>
2023-05-17 14:33:44 +0200

    Quote variables

--- backup.sh
+++ backup.sh
@@ -5,23 +5,23 @@ set -e
 # Get secrets from files
 # Insecure, but there's not much I can do about it
 # It's duplicity's fault!
-export PASSPHRASE=$(cat ${DUPLICITY_PASSPHRASE_FILE})
+export PASSPHRASE=$(cat "${DUPLICITY_PASSPHRASE_FILE}")
 
 case "$MODE" in
 	backup)
 		echo "Launched in backup mode, performing backup..." >> /dev/stderr
 		duplicity \
 			--allow-source-mismatch \
-			--full-if-older-than ${DUPLICITY_FULL_IF_OLDER_THAN} \
+			--full-if-older-than "${DUPLICITY_FULL_IF_OLDER_THAN}" \
 			/mnt \
-			${DUPLICITY_TARGET_URL}
+			"${DUPLICITY_TARGET_URL}"
 	;;
 	restore)
 		echo "Launched in restore mode, restoring backup..." >> /dev/stderr
 		duplicity \
 			--force \
 			--allow-source-mismatch \
-			${DUPLICITY_TARGET_URL} \
+			"${DUPLICITY_TARGET_URL}" \
 			/mnt
 	;;
 	*)
