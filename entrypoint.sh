16151233a239b867253035eee7ba592da34bbf0d
Stefano Pigozzi <me@steffo.eu>
2023-06-18 21:32:31 +0200

    Tweak entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ case "$MODE" in
     /etc/periodic/daily/backup.sh
     echo "Running cron for daily backups..."
     crond -f -l 0
+    echo "Cron has exited."
 	;;
 	restore)
     echo "Restoring from latest backup..."
     /usr/lib/backup-duplicity/restore.sh
+    echo "Done."
 	;;
 	*)
 		echo "No such mode." >> /dev/stderr
