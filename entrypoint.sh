116026c5e08076652debd4e01ea76edc18ee5a25
Stefano Pigozzi <me@steffo.eu>
2024-06-05 09:13:15 +0200

    Indent entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 case "$MODE" in
 	backup)
-    echo "Running first backup..."
-    /etc/periodic/daily/backup.sh
-    echo "Running cron for daily backups..."
-    crond -f -l 0
-    echo "Cron has exited."
+        echo "Running first backup..."
+        /etc/periodic/daily/backup.sh
+        echo "Running cron for daily backups..."
+        crond -f -l 0
+        echo "Cron has exited."
 	;;
 	restore)
-    echo "Restoring from latest backup..."
-    /usr/lib/backup-duplicity/restore.sh
-    echo "Done."
+        echo "Restoring from latest backup..."
+        /usr/lib/backup-duplicity/restore.sh
+        echo "Done."
 	;;
 	*)
 		echo "No such mode." >> /dev/stderr
